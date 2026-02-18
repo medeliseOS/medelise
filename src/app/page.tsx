766c9d7c8fd30b7ae536f5c1ebc0896d0fa62812
MIRCEA POPA <mi@MacBook-Air-MIRCEA.local>
2026-02-18 08:44:55 +0200

    fix: move HowItWorksSection after EasyCareSection (membership)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
       <TestimonialsSection />
       <PatientPortalSection />
       <ServiciiSection />
-      <HowItWorksSection />
       <EasyCareSection />
+      <HowItWorksSection />
     </main>
   );
 }
